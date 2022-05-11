37ab6072a53579560eb5e694b8183cdcafd81d25
Jwade1781 <jwade5219@gmail.com>
2022-05-10 21:46:48 -0400

    Further improved UI

---
+++ src/main.js
@@ -0,0 +1,25 @@
+
+
+window.onload = () => {
+    createClouds();
+}
+
+
+function createClouds(){
+    const TOTAL = 3;
+    const pictureContainer = document.querySelector(".picture");
+
+    /*for (let i = 0; i < TOTAL; i++) {
+        let cloud = document.createElement("div");
+        cloud.classList.add("cloud");
+        let rotate = Math.random(1) * 5;
+        cloud.style.transform = ("rotate(" + rotate + "deg");
+        cloud.style.transform = ("scale(" + rotate + ")")
+        pictureContainer.appendChild(cloud);
+        console.log("Added cloud" + rotate);
+    }*/
+}
+
+function events () {
+    
+}
